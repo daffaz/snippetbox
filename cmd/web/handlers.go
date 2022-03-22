1693b9af52146448f025f4ea9d8bcc9898b39d2e
Daffa Z <daffazakyy8@gmail.com>
2022-03-23 02:06:51 +0700

    next template actions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -64,7 +65,21 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	fmt.Fprintf(w, "%v", s)
+	data := &templateData{Snippet: s}
+	files := []string{
+		"./ui/html/show.page.gohtml",
+		"./ui/html/base.layout.gohtml",
+		"./ui/html/footer.partial.gohtml",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+	}
+
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/daffaz/snippetbox/pkg/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
